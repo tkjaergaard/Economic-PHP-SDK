3502aee0a9053e40320eda70a2a14d20f0e3f1c2
Henrik Ankersø <privat@diblo.dk>
2015-04-17 18:18:18 +0200

    A class to handle price groups
    The start of implementing a class to handle price groups (Special price)

---
+++ src/tkj/Economics/PriceGroup/PriceGroup.php
@@ -0,0 +1,47 @@
+<?php namespace custom\Economics;
+
+use tkj\Economics\Client;
+
+class PriceGroup {
+
+    /**
+     * Client Connection
+     * @var devdk\Economics\Client
+     */
+    protected $client;
+
+    /**
+     * Construct class and set dependencies
+     * @param devdk\Economics\Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client     = $client->getClient();
+    }
+
+    /**
+     * Return all price groups
+     * @return array
+     */
+    public function all()
+    {
+        return $this->client
+                    ->PriceGroup_GetAll()
+					->PriceGroup_GetAllResult
+					->PriceGroupHandle;
+    }
+
+    /**
+     * Returns the price for the product in the PriceGroup
+     * If no special price is found then the sales price is returned instead
+     * @param  integer $groupNo
+     * @param  string   $productNo
+     * @return float
+     */
+    public function getPrice($groupNo, $productNo)
+    {
+        return $this->client
+                    ->PriceGroup_GetPrice(array('priceGroupHandle'=>array('Number'=>$groupNo), 'productHandle'=>array('Number'=>$productNo)))
+					->PriceGroup_GetPriceResult;
+    }
+}
