3215edc03725d4d49213b1eee12609df7b57d681
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2016-03-14 12:39:30 +0100

    Corrected typos & PSR-2

--- src/tkj/Economics/TokenClient.php
+++ src/tkj/Economics/TokenClient.php
@@ -1,35 +1,59 @@
-<?php namespace tkj\Economics;
+<?php
+
+namespace tkj\Economics;
 
 use SoapClient;
 
-class TokenClient implements ClientInterface {
-
+class TokenClient implements ClientInterface
+{
     use ClientableTrait;
 
+    /**
+     * @var string
+     */
     protected $token;
 
+    /**
+     * @var string
+     */
     protected $appToken;
 
+    /**
+     * @var string
+     */
     protected $appIdentifier;
 
+    /**
+     * @param string $token
+     * @param string $appToken
+     * @param string $appIdentifier
+     */
     public function __construct($token, $appToken, $appIdentifier)
     {
         $this->token = $token;
         $this->appToken = $appToken;
         $this->appIdentifier = $appIdentifier;
 
-        $this->setupAppIndentifierContex();
+        $this->setupAppIdentifierContext();
 
         $this->client = $this->setupClient();
     }
 
-    protected function setupAppIndentifierContex()
+    /**
+     * Setup app identifier context
+     */
+    protected function setupAppIdentifierContext()
     {
         $this->debug['stream_context'] = stream_context_create([
            'http' => ['header' => 'X-EconomicAppIdentifier: ' . $this->appIdentifier]
         ]);
     }
 
+    /**
+     * Setup client
+     *
+     * @return SoapClient
+     */
     protected function setupClient()
     {
         $client = new SoapClient($this->apiUrl, $this->debug);
